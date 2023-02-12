a885b19ca67940d4c31a98bd393c86e083f2afd7
Chris Leonard <chrisleonard.05@gmail.com>
2023-02-11 23:33:47 -0600

    Think I mostly figured out the Hinge Joint 2D. Created a couple more "hooks" to latch onto.

--- Assets/TongueMechanicsScript.cs
+++ Assets/TongueMechanicsScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class TongueMechanicsScript : MonoBehaviour
 {
@@ -9,17 +11,21 @@ public class TongueMechanicsScript : MonoBehaviour
     public GameObject tongue;
     // Start is called before the first frame update
     private HingeJoint2D _frogHingeJoint2D;
+
+    private Vector2 connectedTonguePoint;
+
+    private bool tongueShouldBeAnchored;
     void Start()
     {
         _frogHingeJoint2D = tongue.GetComponent<HingeJoint2D>();
         //move the anchorpoint to match the frog's position on screen
-        Vector2 frogPos = frog.transform.position;
-        Vector2 tonguePos = _frogHingeJoint2D.anchor;
-        tonguePos.x = frogPos.x;
-        tonguePos.y = frogPos.y;
-        Vector2 connector = _frogHingeJoint2D.connectedAnchor;
-        connector.x = frogPos.x;
-        connector.y = frogPos.y;
+        // Vector2 frogPos = frog.transform.position;
+        // Vector2 tonguePos = _frogHingeJoint2D.anchor;
+        // tonguePos.x = frogPos.x;
+        // tonguePos.y = frogPos.y;
+        // Vector2 connector = _frogHingeJoint2D.connectedAnchor;
+        // connector.x = frogPos.x;
+        // connector.y = frogPos.y;
     }
 
     // Update is called once per frame
@@ -28,12 +34,12 @@ public class TongueMechanicsScript : MonoBehaviour
         var rayCast = getTouchHit();
         if (rayCast.collider != null)
         {
-            var hitPoints = rayCast.point;
-            _frogHingeJoint2D.connectedAnchor = hitPoints;
+            connectedTonguePoint = rayCast.point;
             _frogHingeJoint2D.enabled = true;
+            tongueShouldBeAnchored = true;
+            tongue.transform.position = connectedTonguePoint;
+            _frogHingeJoint2D.anchor = Vector2.zero;
         }
-        
-
     }
 
     private RaycastHit2D getTouchHit()
