37e534270f8be014f2205fb562ab5f8cad0320a7
Chris Leonard <chrisleonard.05@gmail.com>
2023-02-11 23:45:40 -0600

    Restructure directories

---
+++ Assets/_Scripts/Game/Controls.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace _Scripts.Game
+{
+    public class Controls : MonoBehaviour
+    {
+        public float jumpForce = 10f;
+        private Rigidbody2D _rigidbody2D;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (UnityEngine.Input.touchCount > 0)
+            {
+                Touch touch = UnityEngine.Input.GetTouch(0);
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                touchPosition.z = 0;
+                Vector2 direction = (Vector2)((touchPosition - transform.position));
+                if (touch.phase == TouchPhase.Began)
+                {
+                    if (touchPosition.x > transform.position.x)
+                    {
+                        _rigidbody2D.AddForce(new Vector2(jumpForce, jumpForce), ForceMode2D.Impulse);
+                    }
+                    else if (touchPosition.x < transform.position.x)
+                    {
+                        _rigidbody2D.AddForce(new Vector2(-jumpForce, jumpForce), ForceMode2D.Impulse);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Game/TongueMechanicsScript.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace _Scripts.Game
+{
+    public class TongueMechanicsScript : MonoBehaviour
+    {
+        public GameObject frog;
+
+        public GameObject tongue;
+        // Start is called before the first frame update
+        private HingeJoint2D _frogHingeJoint2D;
+
+        private Vector2 connectedTonguePoint;
+
+        private bool tongueShouldBeAnchored;
+        void Start()
+        {
+            _frogHingeJoint2D = tongue.GetComponent<HingeJoint2D>();
+            //move the anchorpoint to match the frog's position on screen
+            // Vector2 frogPos = frog.transform.position;
+            // Vector2 tonguePos = _frogHingeJoint2D.anchor;
+            // tonguePos.x = frogPos.x;
+            // tonguePos.y = frogPos.y;
+            // Vector2 connector = _frogHingeJoint2D.connectedAnchor;
+            // connector.x = frogPos.x;
+            // connector.y = frogPos.y;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var rayCast = getTouchHit();
+            if (rayCast.collider != null)
+            {
+                connectedTonguePoint = rayCast.point;
+                _frogHingeJoint2D.enabled = true;
+                tongueShouldBeAnchored = true;
+                tongue.transform.position = connectedTonguePoint;
+                _frogHingeJoint2D.anchor = Vector2.zero;
+            }
+        }
+
+        private RaycastHit2D getTouchHit()
+        {
+            if (Input.touchCount > 0)
+            {
+                var touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Vector2 touchPosition = touch.position;
+                    Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+                    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+                    return hit;
+                }
+            }
+
+            return new RaycastHit2D();
+        }
+    }
+}
