f53558ea3ad35073e92e299929f4b73e061477d5
James <james.andrews23@gmail.com>
2023-02-14 20:48:04 -0600

    Prevents jumping unless frog is colliding with a tagged "Platform". This prevents the double jump while in the air, or while the frog was swinging by the tongue.

--- Assets/_Scripts/Game/Controls.cs
+++ Assets/_Scripts/Game/Controls.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace _Scripts.Game
@@ -6,6 +7,7 @@ namespace _Scripts.Game
     {
         public float jumpForce = 10f;
         private Rigidbody2D _rigidbody2D;
+        private bool _isJumping;
 
         // Start is called before the first frame update
         void Start()
@@ -14,24 +16,33 @@ namespace _Scripts.Game
             _rigidbody2D.freezeRotation = true; //adding this to prevent the frog's body from rotating while tongue is attached
         }
 
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Platform"))
+            {
+                _isJumping = false;
+            }
+        }
+
         // Update is called once per frame
         void Update()
         {
-            if (UnityEngine.Input.touchCount > 0)
+            if (Input.touchCount > 0)
             {
-                Touch touch = UnityEngine.Input.GetTouch(0);
+                Touch touch = Input.GetTouch(0);
                 Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
                 touchPosition.z = 0;
-                Vector2 direction = (Vector2)((touchPosition - transform.position));
-                if (touch.phase == TouchPhase.Began)
+                if (touch.phase == TouchPhase.Began && !_isJumping)
                 {
                     if (touchPosition.x > transform.position.x)
                     {
                         _rigidbody2D.AddForce(new Vector2(jumpForce, jumpForce), ForceMode2D.Impulse);
+                        _isJumping = true;
                     }
                     else if (touchPosition.x < transform.position.x)
                     {
                         _rigidbody2D.AddForce(new Vector2(-jumpForce, jumpForce), ForceMode2D.Impulse);
+                        _isJumping = true;
                     }
                 }
             }
