01cab05dcf584ae7f0fed1268b25cd38ea02e5a0
James <james.andrews23@gmail.com>
2023-02-16 13:03:13 -0600

    Frog launcher. Drag and release the frog while he's hanging and he'll go flying.

--- Assets/_Scripts/Game/Controls.cs
+++ Assets/_Scripts/Game/Controls.cs
@@ -8,12 +8,16 @@ namespace _Scripts.Game
         public float jumpForce = 10f;
         private Rigidbody2D _rigidbody2D;
         private bool _isJumping;
+        private Touch _touch;
+        public GameObject frogTongue;
+        private bool _isDragging;
 
         // Start is called before the first frame update
         void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _rigidbody2D.freezeRotation = true; //adding this to prevent the frog's body from rotating while tongue is attached
+            _isDragging = false;
         }
 
         private void OnCollisionEnter2D(Collision2D col)
@@ -29,23 +33,79 @@ namespace _Scripts.Game
         {
             if (Input.touchCount > 0)
             {
-                Touch touch = Input.GetTouch(0);
-                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                _touch = Input.GetTouch(0);
+                Debug.Log("Touch Phase: " + _touch.phase);
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(_touch.position);
                 touchPosition.z = 0;
-                if (touch.phase == TouchPhase.Began && !_isJumping)
+
+                RaycastHit2D ray = GetTouchHit();
+                SpringJoint2D tongue = frogTongue.GetComponent<SpringJoint2D>();
+                bool isTargetFrogAndAttached = ray.collider && ray.collider.gameObject == gameObject && tongue.enabled;
+                
+                Debug.Log("Dragging: " + _isDragging);
+                Debug.Log("Frog Target: " + isTargetFrogAndAttached);
+
+                if (_touch.phase == TouchPhase.Began && !_isJumping)
                 {
-                    if (touchPosition.x > transform.position.x)
-                    {
-                        _rigidbody2D.AddForce(new Vector2(jumpForce, jumpForce), ForceMode2D.Impulse);
-                        _isJumping = true;
-                    }
-                    else if (touchPosition.x < transform.position.x)
-                    {
-                        _rigidbody2D.AddForce(new Vector2(-jumpForce, jumpForce), ForceMode2D.Impulse);
-                        _isJumping = true;
-                    }
+                    Jump(touchPosition);
+                }
+                
+                if (_touch.phase == TouchPhase.Began && isTargetFrogAndAttached)
+                {
+                    _isDragging = true;
+                }
+
+                if (_isDragging)
+                {
+                    Dragging();
+                }
+
+                if (_touch.phase == TouchPhase.Ended && isTargetFrogAndAttached)
+                {
+                    _isDragging = false;
+                    tongue.enabled = false;
                 }
             }
+        }
+
+        private void Jump(Vector3 touchPosition)
+        {
+            if (touchPosition.x > transform.position.x)
+            {
+                _rigidbody2D.AddForce(new Vector2(jumpForce, jumpForce), ForceMode2D.Impulse);
+                _isJumping = true;
+            }
+            else if (touchPosition.x < transform.position.x)
+            {
+                _rigidbody2D.AddForce(new Vector2(-jumpForce, jumpForce), ForceMode2D.Impulse);
+                _isJumping = true;
+            }
+        }
+
+        private void Dragging()
+        {
+            Vector3 draggingPos = Camera.main.ScreenToWorldPoint(_touch.position);
+            draggingPos.z = 0f;
+            transform.position = draggingPos;
+        }
+        
+        private RaycastHit2D GetTouchHit()
+        {
+            if (Input.touchCount > 0)
+            {
+                var touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Ended)
+                {
+                    Vector2 touchPosition = touch.position;
+                    Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+                    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+                    return hit;
+                }
+            }
+
+            return new RaycastHit2D();
         }
     }
 }
