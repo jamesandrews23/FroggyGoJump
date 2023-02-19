38fadf0158157b005dce76b98b430ce411a7ab02
James <james.andrews23@gmail.com>
2023-02-18 19:02:31 -0600

    Allows frog to jump through the platform, but then collide with the platform's top. Creates a prefab from the platform.

---
+++ Assets/Platform.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Frog"))
+        {
+            Rigidbody2D frog = col.GetComponent<Rigidbody2D>();
+
+            if (frog.velocity.y > 0)
+            {
+                var box2d = gameObject.GetComponent<BoxCollider2D>();
+                box2d.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Frog"))
+        {
+            var box2d = gameObject.GetComponent<BoxCollider2D>();
+            box2d.enabled = true;
+        }
+    }
+}
