0224b88ebd64f5855c1a15a8ee6e9a8d3e9feca2
James <james.andrews23@gmail.com>
2023-02-20 17:14:29 -0600

    First attempt at a level generator.

---
+++ Assets/_Scripts/Level/LevelSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LevelSpawner : MonoBehaviour
+{
+    public List<Transform> levelParts;
+    public GameObject player;
+    private Vector3 _lastPos;
+    private const float PlayerDistSpawnLevelPart = 200f;
+    private float _leftBorder;
+    private float _rightBorder;
+    private void Awake()
+    {
+        _leftBorder = FindLeftScreenBorder();
+        _rightBorder = FindRightScreenBorder();
+        
+        _lastPos = SpawnLevelPart(player.transform.position + new Vector3(0, -.5f, 0), levelParts[0]).position;
+
+        SpawnLevelPart();
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     SpawnLevelPart();
+        // }
+    }
+
+    private void Update()
+    {
+        //todo where magnitude
+        if (Vector3.Distance(player.transform.position, _lastPos) < PlayerDistSpawnLevelPart)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+    private void SpawnLevelPart()
+    {
+        Vector3 newPos = _lastPos;
+        //if x was closer to the left, then move to the right, and vice-a-versa
+        float nextPos = Random.Range(_leftBorder, _rightBorder);
+        newPos.x = nextPos;
+        Transform chosenPart = levelParts[Random.Range(0, levelParts.Count)];
+        Transform spawnedPart = SpawnLevelPart(newPos, chosenPart);
+        _lastPos = spawnedPart.Find("EndPos").position;
+    }
+
+    private Transform SpawnLevelPart(Vector3 position, Transform levelPart)
+    {
+        return Instantiate(levelPart, position, Quaternion.identity);
+    }
+
+    private float FindRightScreenBorder()
+    {
+        Camera mainCamera = Camera.main;
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+        return mainCamera.transform.position.x + cameraWidth / 2f;
+    }
+    
+    private float FindLeftScreenBorder()
+    {
+        Camera mainCamera = Camera.main;
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+        return mainCamera.transform.position.x - cameraWidth / 2f;
+    }
+}
