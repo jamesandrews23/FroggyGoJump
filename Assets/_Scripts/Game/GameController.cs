b602acf72e57cd23bf0b63c67acc1ce86823b172
James <james.andrews23@gmail.com>
2023-02-25 11:02:19 -0600

    Checks if the player's SpringJoint2D is active, and if so does not end game if player falls below bottom of screen.

--- Assets/_Scripts/Game/GameController.cs
+++ Assets/_Scripts/Game/GameController.cs
@@ -1,35 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using _Scripts.Game;
 using _Scripts.Game.InputControl;
 using _Scripts.Game.SceneManagement;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+namespace _Scripts.Game
 {
-    public GameOverScreen gameOverScreen;
-    // Start is called before the first frame update
-    public GameObject player;
-
-    private Controls frogControls;
-
-    private void GameOver()
+    public class GameController : MonoBehaviour
     {
-        gameOverScreen.Setup((int) player.GetComponent<Controls>().maxHeightReached);
-    }
-    void Start()
-    {
-        frogControls = player.GetComponent<Controls>();
-    }
+        public GameOverScreen gameOverScreen;
+        // Start is called before the first frame update
+        public GameObject player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float cameraBottomY = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane)).y;
-        if (!frogControls.IsDragging && player.transform.position.y < cameraBottomY)
+        private Controls _frogControls;
+
+        private void GameOver()
         {
-            player.SetActive(false);
-            GameOver();
+            gameOverScreen.Setup((int) player.GetComponent<Controls>().maxHeightReached);
+        }
+        void Start()
+        {
+            _frogControls = player.GetComponent<Controls>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float cameraBottomY = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane)).y;
+            bool isPlayerAttached = _frogControls.frogTongue.GetComponent<SpringJoint2D>().isActiveAndEnabled;
+            if (!_frogControls.IsDragging && player.transform.position.y < cameraBottomY && !isPlayerAttached)
+            {
+                player.SetActive(false);
+                GameOver();
+            }
         }
     }
 }
