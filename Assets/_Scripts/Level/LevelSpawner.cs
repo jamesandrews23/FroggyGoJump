b11fc75a4dc8e428d0490323c37ba520e1d6603e
James <james.andrews23@gmail.com>
2023-02-25 18:12:01 -0600

    Uses the empty game object transform included in each prefab platform as the next position. Checks the new position's x, and if it is outside of the bounds of the screen, then picks a random position between the screen borders and sets that to x.

--- Assets/_Scripts/Level/LevelSpawner.cs
+++ Assets/_Scripts/Level/LevelSpawner.cs
@@ -1,72 +1,73 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class LevelSpawner : MonoBehaviour
+namespace _Scripts.Level
 {
-    public List<Transform> levelParts;
-    public GameObject player;
-    private Vector3 _lastLevelPartPos;
-    private const float PlayerDistSpawnLevelPart = 15f;
-    private float _leftBorder;
-    private float _rightBorder;
-    public float offsetY = 100f;
-    private void Awake()
+    public class LevelSpawner : MonoBehaviour
     {
-        _leftBorder = FindLeftScreenBorder();
-        _rightBorder = FindRightScreenBorder();
-        
-        _lastLevelPartPos = SpawnLevelPart(player.transform.position + new Vector3(0, -.5f, 0), levelParts[0]).position;
-
-        SpawnLevelPart();
-        // for (int i = 0; i < 5; i++)
-        // {
-        //     SpawnLevelPart();
-        // }
-    }
-
-    private void Update()
-    {
-        //todo where magnitude
-        if (Vector3.Distance(player.transform.position, _lastLevelPartPos) < PlayerDistSpawnLevelPart)
+        public List<Transform> levelParts;
+        public GameObject player;
+        private Transform _lastLevelPartPos;
+        private const float PlayerDistSpawnLevelPart = 15f;
+        private float _leftBorder;
+        private float _rightBorder;
+        private void Awake()
         {
+            _leftBorder = FindLeftScreenBorder();
+            _rightBorder = FindRightScreenBorder();
+        
+            _lastLevelPartPos = SpawnLevelPart(player.transform.position + new Vector3(0, -.5f, 0), levelParts[0]);
+
             SpawnLevelPart();
         }
-    }
 
-    private void SpawnLevelPart()
-    {
-        Vector3 newPos = _lastLevelPartPos;
-        //if x was closer to the left, then move to the right, and vice-a-versa
-        Transform chosenPart = levelParts[Random.Range(0, levelParts.Count)];
-        float nextPos = Random.Range(_leftBorder, _rightBorder);
-        newPos.x = nextPos - chosenPart.GetComponent<Renderer>().bounds.size.x / 2.0f;
-        newPos.y = _lastLevelPartPos.y + offsetY;
-        // newPos.x = Vector3.Slerp(new Vector3(_leftBorder, 0, 0), new Vector3(_rightBorder, 0, 0), Random.Range(0f, 1f)).x;
-        _lastLevelPartPos = SpawnLevelPart(newPos, chosenPart).position;
-    }
+        private void Update()
+        {
+            //todo where magnitude
+            if (Vector3.Distance(player.transform.position, _lastLevelPartPos.position) < PlayerDistSpawnLevelPart)
+            {
+                SpawnLevelPart();
+            }
+        }
 
-    private Transform SpawnLevelPart(Vector3 position, Transform levelPart)
-    {
-        return Instantiate(levelPart, position, Quaternion.identity);
-    }
+        private void SpawnLevelPart()
+        {
+            Vector3 newPos = _lastLevelPartPos.Find("EndPos").position;
+            Transform chosenPart = levelParts[Random.Range(0, levelParts.Count)];
 
-    private float FindRightScreenBorder()
-    {
-        Camera mainCamera = Camera.main;
-        float cameraHeight = 2f * mainCamera.orthographicSize;
-        float cameraWidth = cameraHeight * mainCamera.aspect;
-        return mainCamera.transform.position.x + cameraWidth / 2f;
-    }
+            if (IsNewPosOutOfBounds(newPos.x)) //outside of the screen area
+            {
+                float randomPosBetweenBounds = Random.Range(_leftBorder, _rightBorder);
+                newPos.x = randomPosBetweenBounds;
+            }
+            _lastLevelPartPos = SpawnLevelPart(newPos, chosenPart);
+        }
+
+        private Transform SpawnLevelPart(Vector3 position, Transform levelPart)
+        {
+            return Instantiate(levelPart, position, Quaternion.identity);
+        }
+
+        private float FindRightScreenBorder()
+        {
+            Camera mainCamera = Camera.main;
+            float cameraHeight = 2f * mainCamera.orthographicSize;
+            float cameraWidth = cameraHeight * mainCamera.aspect;
+            return mainCamera.transform.position.x + cameraWidth / 2f;
+        }
     
-    private float FindLeftScreenBorder()
-    {
-        Camera mainCamera = Camera.main;
-        float cameraHeight = 2f * mainCamera.orthographicSize;
-        float cameraWidth = cameraHeight * mainCamera.aspect;
-        return mainCamera.transform.position.x - cameraWidth / 2f;
+        private float FindLeftScreenBorder()
+        {
+            Camera mainCamera = Camera.main;
+            float cameraHeight = 2f * mainCamera.orthographicSize;
+            float cameraWidth = cameraHeight * mainCamera.aspect;
+            return mainCamera.transform.position.x - cameraWidth / 2f;
+        }
+
+        private bool IsNewPosOutOfBounds(float newPos)
+        {
+            return newPos < _leftBorder || newPos > _rightBorder;
+        }
     }
 }
