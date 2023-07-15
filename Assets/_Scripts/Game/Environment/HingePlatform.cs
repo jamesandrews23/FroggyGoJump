3bebb519445908d58159f8cc2a640451ff0e0540
James <james.andrews23@gmail.com>
2023-07-15 15:14:25 -0500

    Returns the platform to its original position after dropped.

--- Assets/_Scripts/Game/Environment/HingePlatform.cs
+++ Assets/_Scripts/Game/Environment/HingePlatform.cs
@@ -7,16 +7,44 @@ using UnityEngine;
 public class HingePlatform : Platform
 {    
     public float dropDelay = 1f;
+    public bool hasDropped = false;
+    private Rigidbody2D rb;
+    private HingeJoint2D joint;
 
-    private void OnCollisionEnter2D(Collision2D col){
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        joint = gameObject.GetComponent<HingeJoint2D>();
+    }
+
+    void Update()
+    {
+        if(hasDropped){
+            StartCoroutine(DelayedReplace());
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
         if(col.gameObject.CompareTag("Frog")){
             StartCoroutine(DelayedDrop());
         }
     }
 
-    private IEnumerator DelayedDrop(){
+    private IEnumerator DelayedDrop()
+    {
         yield return new WaitForSeconds(dropDelay);
-        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
         rb.bodyType = RigidbodyType2D.Dynamic;
+        hasDropped = true;
+    }
+
+    private IEnumerator DelayedReplace()
+    {
+        yield return new WaitForSeconds(dropDelay);
+        hasDropped = false;
+        joint.useMotor = true;
+        JointMotor2D motor = joint.motor;
+        motor.motorSpeed = 100;
+        joint.motor = motor;
     }
 }
